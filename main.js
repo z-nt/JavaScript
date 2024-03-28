89a690bc3743ca1b964078ee01bc08065ee056c3
kian_nrt7 <kiannosratian@gmail.com>
2024-03-28 21:56:51 +0330

    some practice added

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 const numbers = [ 1,2,3,4,5,6,7,8,9];
 
 function getMinNumbers(arr){
@@ -55,6 +56,145 @@ function converF_To_c(startStep,endStep,step,C){
 }
 
 //converF_To_c(10,20,1,40);
+
+
+function calculator(x,y,opt){
+    if(opt == "+") return x + y;
+    if(opt == "-") return x -y;
+    if(opt == "x") return x * y;
+}
+//calculator(5,6,"+");
+function rockPaper(x,y){
+    var user = 0;
+    var computer= 0;
+    var winner =3;
+    
+    while(winner){
+    
+    if(x==y) return false;
+    if(x == "paper" && y == "rock") user++;
+    if(x== "rock" && y == "paper") computer++;
+    if(user == winner) console.log("user winn");
+    if(computer == winner) console.log("coumputer winn");
+    }
+    }
+
+//rockPaper("paper","rock");
+
+class Machine{
+    counstructor(name,model,color){
+        this.name = name;
+        this.model = model;
+        this.color = color;
+    }
+}
+//const ford = new Machine("ford","1996","gray");
+
+// const Person = {
+//     name : "kian",
+//     age : 26,
+//     work : "programer"
+// }
+
+// const ali = Person;
+// ali.name = "ali";
+// ali.age = 20;
+// ali.work = "designer";
+
+// //console.log(ali);
+
+// import person from "./main.json" assert {type : "json"};
+
+// console.log(person);
+// function fetchJSONData() {
+//     fetch("./main.json")
+//         .then((res) => {
+//             if (!res.ok) {
+//                 throw new Error
+//                     (`HTTP error! Status: ${res.status}`);
+//             }
+//             return res.json();
+//         })
+//         .then((data) => 
+//               console.log(data))
+//         .catch((error) => 
+//                console.error("Unable to fetch data:", error));
+// }
+// fetchJSONData();
+
+// function getLocation(){
+    //     if(navigator.geolocation){
+        //         navigator.geolocation.getCurrentPosition(showPosition);
+        //     }else{
+            //         demo.innerHTML =" gea is not ";
+            //     }
+// }
+
+// function showPosition(position){
+    //     demo.innerHTML = "latidue: " + position.cords.latitude + "<br>long" + position.cords.longitude;
+    // }
+    
+// const text = '{"name":"John", "birth":"1986-12-14", "city":"New York"}';
+// const demo = document.getElementById("demo");
+
+// const obj = JSON.parse(text,function(key,value){
+//         if(key == "birth"){
+//             return new Data(value);
+//         }else{
+//             return value;
+//         }
+// })
+// demo.innerHTML= obj.birth;
+// console.log(demo);
+
+// const person= {
+//     name:"kian",
+//     age : 26,
+//     work : "programmer"
+// }
+
+// const myJson = JSON.stringify(person);
+// window.location = "demo_json.php?x=" + myJson;
+
+
+// const myJson = JSON.stringify(person)
+// localStorage.setItem("testJson",myJson);
+
+
+// const text = localStorage.getItem("testJson");
+// const obj = JSON.parse(text);
+// let  word= ""
+// for(const x in obj){
+//     word +=x + " ";
+// }
+// console.log(word);
+
+const https = new XMLHttpRequest();
+https.onload = function(){
+    console.log(this.responseText);
+    const myObject = JSON.parse(this.responseText);
+    document.getElementById("demo").innerHTML = myObject.name;
+}
+https.open("GET","kian.txt");
+https.send();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
