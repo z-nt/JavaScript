11d49a496434f13dd3c986484ed55898c5f1463a
kian_nrt7 <kiannosratian@gmail.com>
2024-04-29 17:48:37 +0330

    html / css inserted

---
+++ linkList/main.js
@@ -0,0 +1,14 @@
+class User{
+    constructor(name,lastName,email,work){
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.work = work;
+    }
+}
+
+const inputName= document.getElementById("name")
+const lastName= document.getElementById("lastname")
+const email= document.getElementById("Email")
+const work= document.getElementById("work")
+const submit = document.getElementById("submit")
